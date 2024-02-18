71318523437a552e1efe9b7ff32be060292281a4
suyoung <suyoung@DESKTOP-SBEEDLG>
2024-02-18 16:38:36 +0900

    필요없는것들 싸그리 싹싹 그리그리 싹싹 추려냄

--- resource/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="2024 tileset" tilewidth="16" tileheight="16" tilecount="120" columns="12">
- <image source="G:/2024/image/kenney_pixelshmup/Tilemap/tiles_packed.png" trans="000000" width="192" height="160"/>
-</tileset>
